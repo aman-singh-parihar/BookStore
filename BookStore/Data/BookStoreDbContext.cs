af14ac76167cae72a241a866a8ee2dde41a8e323
Aman <amanparihar16@gmail.com>
2023-11-22 11:01:45 +0530

    first commit

---
+++ BookStore/Data/BookStoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.Data
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        {
+        }
+
+        
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BookStore/Program.cs
@@ -0,0 +1,37 @@
+using BookStore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var configuration = builder.Configuration;
+            var services = builder.Services;
+            services.AddControllersWithViews();
+
+            services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
